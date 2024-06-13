936e141742a6d5ba344419a1f41f118821fb473d
Alejandro Torres <torresalexatlanta@gmail.com>
2024-06-12 18:35:24 -0600

    correccion de cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,57 @@ import Options from "./components/Options";
 import DescriptionCards from "./components/DescriptionCards";
 
 export default function App() {
+  const cards = [
+    {
+      icon: "💡",
+      title: "Instant Server Start",
+      text: "On demand file serving over native ESM, no bundling required!",
+    },
+    {
+      icon: "⚡️",
+      title: "Lightning Fast HMR",
+      text: "Hot Module Replacement (HMR) that stays fast regardless of app size.",
+    },
+    {
+      icon: "🛠️",
+      title: "Rich Features",
+      text: "Out-of-the-box support for TypeScript, JSX, CSS and more.",
+    },
+    {
+      icon: "📦",
+      title: "Optimized Build",
+      text: "Pre-configured Rollup build with multi-page and library mode support.",
+    },
+    {
+      icon: "🔩",
+      title: "Universal Plugins",
+      text: "Rollup-superset plugin interface shared between dev and build.",
+    },
+
+    {
+      icon: "🔑",
+      title: "Fully Typed APIs",
+      text: "Flexible programmatic APIs with full TypeScript typing.",
+    },
+  ];
   return (
     <main id="main-layout">
       <Introduction />
       <ViteLog />
       <Options />
-      <DescriptionCards />
+      <section className="card-section">
+        {cards.map((card) => {
+          return (
+            <DescriptionCards
+              className="cards"
+              key={`card-${card.title}`}
+              icon={card.icon}
+              title={card.title}
+              text={card.text}
+            />
+          );
+        })}
+      </section>
     </main>
   );
 }
--- src/components/DescriptionCards.jsx
+++ src/components/DescriptionCards.jsx
@@ -1,48 +1,11 @@
-export default function DescriptionCards() {
-  const cards = [
-    {
-      icon: "💡",
-      title: "Instant Server Start",
-      text: "On demand file serving over native ESM, no bundling required!",
-    },
-    {
-      icon: "⚡️",
-      title: "Lightning Fast HMR",
-      text: "Hot Module Replacement (HMR) that stays fast regardless of app size.",
-    },
-    {
-      icon: "🛠️",
-      title: "Rich Features",
-      text: "Out-of-the-box support for TypeScript, JSX, CSS and more.",
-    },
-    {
-      icon: "📦",
-      title: "Optimized Build",
-      text: "Pre-configured Rollup build with multi-page and library mode support.",
-    },
-    {
-      icon: "🔩",
-      title: "Universal Plugins",
-      text: "Rollup-superset plugin interface shared between dev and build.",
-    },
-
-    {
-      icon: "🔑",
-      title: "Fully Typed APIs",
-      text: "Flexible programmatic APIs with full TypeScript typing.",
-    },
-  ];
+export default function DescriptionCards({ icon, title, text }) {
   return (
-    <section className="card-section">
-      {cards.map((card) => {
-        return (
-          <article key={`card-${card.title}`} className="cards">
-            <span className="card-icon">{card.icon}</span>
-            <h4 className="card-title">{card.title}</h4>
-            <p className="card-text">{card.text}</p>
-          </article>
-        );
-      })}
+    <section>
+      <article>
+        <span className="card-icon">{icon}</span>
+        <h4 className="card-title">{title}</h4>
+        <p className="card-text">{text}</p>
+      </article>
     </section>
   );
 }
